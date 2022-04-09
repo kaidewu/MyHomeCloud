b76544bd80348f8395c439adebc264f2ecdea65f
Kaide <kaidewu@protonmail.com>
2022-04-09 09:50:46 +0200

    fix linux port hosting

--- Linux/main.py
+++ Linux/main.py
@@ -249,4 +249,4 @@ def create_folder():
         return render_template('404.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
