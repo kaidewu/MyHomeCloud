b7e4607aaae39c6d531287d13408f0e065b9ea27
Kaide <71648366+kaidewu@users.noreply.github.com>
2022-04-12 09:30:57 +0200

    Update main.py

--- Windows/main.py
+++ Windows/main.py
@@ -7,6 +7,8 @@ from hurry.filesize import size
 import os
 import hashlib
 import re
+import ssl
+from flask_sslify import SSLify
 
 # Importamos el contenido de config.env
 from dotenv import load_dotenv
@@ -23,6 +25,7 @@ BASE_DIR = os.environ.get('BASE_DIR')
 abs_path_folder = []
 
 app = Flask(__name__,template_folder='template')
+sslify = SSLify(app)
 app.secret_key = secret_key
 
 # Conexion a la base de datos
@@ -245,4 +248,6 @@ def create_folder():
         return render_template('404.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=443, debug=True, ssl_context='adhoc')
+     context = ssl.Context(SSL.PROTOCOL_TLS_SERVER)
+     context.load_cert_chain('/home/Username/myhomecloud/certs/key.pem', '/home/Username/myhomecloud/certs/cert.pem')
+     app.run(host='0.0.0.0', port=443, debug=True, ssl_context=context)
